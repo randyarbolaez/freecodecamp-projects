3a99d81f5de3011794a60e9091372865411c1823
Randy <randyarbolaez0@gmail.com>
2018-08-23 00:10:17 -0400

    09-add-key-value-pairs-to-javaScript-objects

---
+++ basic-data-structures/09-add-key-value-pairs-to-javaScript-objects.js
@@ -0,0 +1,17 @@
+/*
+Using the same syntax, we can also add new key-value pairs to objects. We've created a foods object with three entries. Add three more entries: bananas with a value of 13, grapes with a value of 35, and strawberries with a value of 27.
+*/
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+};
+
+// change code below this line
+foods.bananas = 13;
+foods.grapes = 35;
+foods.strawberries = 27;
+// change code above this line
+
+console.log(foods);
