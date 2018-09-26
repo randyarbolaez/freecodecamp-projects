021cf8317583678bb836b77d74aecf15cf7af7d5
Randy <randyarbolaez0@gmail.com>
2018-09-26 01:38:41 -0400

    11,12,13,14,15 object oriented programming

---
+++ object-oriented-programming/12-change-the-prototype-to-a-new-object.js
@@ -0,0 +1,18 @@
+/*
+Add the property numLegs and the two methods eat() and describe() to the prototype of Dog by setting the prototype to a new object.
+*/
+
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype = {
+  // Add your code below this line
+  numLegs: 4,
+  eat: function() {
+    console.log('nom nom nom');
+  },
+  describe: function() {
+    console.log(`my dog has ${this.numLegs}`);
+  },
+};
