ae9474b674020770f1ea57b1d453442e46490f7d
Randy <randyarbolaez0@gmail.com>
2018-10-01 00:38:23 -0400

    22,23,24,25 object oriented programming

---
+++ object-oriented-programming/22-use-a-mixin-to-add-common-behavior-between-unrelated-objects.js
@@ -0,0 +1,27 @@
+/*
+Create a mixin named glideMixin that defines a method named glide. Then use the glideMixin to give both bird and boat the ability to glide.
+*/
+
+let bird = {
+  name: 'Donald',
+  numLegs: 2,
+};
+
+let boat = {
+  name: 'Warrior',
+  type: 'race-boat',
+};
+
+// Add your code below this line
+
+let glideMixin = function(obj) {
+  obj.glide = function() {
+    console.log(`whosh`);
+  };
+};
+
+glideMixin(bird);
+glideMixin(boat);
+
+bird.glide();
+boat.glide();
