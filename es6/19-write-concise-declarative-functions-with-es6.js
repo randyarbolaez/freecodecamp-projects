477f9986220cc178b187786796508ef9ace43e6d
Randy <randyarbolaez0@gmail.com>
2018-10-08 00:23:17 -0400

    16,17,18,19,20

---
+++ es6/19-write-concise-declarative-functions-with-es6.js
@@ -0,0 +1,15 @@
+/*
+Refactor the function setGear inside the object bicycle to use the shorthand syntax described above.
+*/
+
+// change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    'use strict';
+    this.gear = newGear;
+  },
+};
+// change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
---
+++ es6/20-use-class-syntax-to-define-a-constructor-function.js
@@ -0,0 +1,20 @@
+/*
+Use class keyword and write a proper constructor to create the Vegetable class.
+
+The Vegetable lets you create a vegetable object, with a property name, to be passed to constructor.
+*/
+
+function makeClass() {
+  'use strict';
+  /* Alter code below this line */
+  class Vegetable {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+  /* Alter code above this line */
+  return Vegetable;
+}
+const Vegetable = makeClass();
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // => should be 'carrot'
