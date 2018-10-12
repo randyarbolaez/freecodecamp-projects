35283f174ce8ed6365dff163249c3027f3da8252
Randy <randyarbolaez0@gmail.com>
2018-10-12 00:39:47 -0400

    06,07,08,09,10 REGEX

---
+++ javascript-algorithims-and-data-structures/regular-expressions/09-match-letters-of-the-alphabet.js
@@ -0,0 +1,10 @@
+/*
+Match all the letters in the string quoteSample.
+
+**Note**
+Be sure to match both upper- and lowercase letters.
+*/
+
+let quoteSample = 'The quick brown fox jumps over the lazy dog.';
+let alphabetRegex = /[a-z]/gi; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
