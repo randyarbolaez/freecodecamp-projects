e588a911c9c17f91f16c049098931d634a2ae274
Randy <randyarbolaez0@gmail.com>
2018-10-30 02:27:45 -0400

    09,10,11,12 BASIC JS

---
+++ javascript-algorithims-and-data-structures/basic-javascript/11-increment-a-number-with-javascript.js
@@ -0,0 +1,11 @@
+/*
+Change the code to use the ++ operator on myVar.
+
+Hint
+Learn more about Arithmetic operators - Increment (++).
+*/
+
+var myVar = 87;
+
+// Only change code below this line
+myVar++;
