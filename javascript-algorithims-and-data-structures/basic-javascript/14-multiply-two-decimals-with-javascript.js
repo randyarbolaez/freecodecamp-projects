071987d7f728313aaba2363dc47ac0117c3a101f
Randy <randyarbolaez0@gmail.com>
2018-10-31 00:54:33 -0400

    13,14,15,16 BASIC JS

---
+++ javascript-algorithims-and-data-structures/basic-javascript/14-multiply-two-decimals-with-javascript.js
@@ -0,0 +1,5 @@
+/*
+Change the 0.0 so that product will equal 5.0.
+*/
+
+var product = 5.0 * 1.0;
---
+++ javascript-algorithims-and-data-structures/basic-javascript/16-finding-a-remainder-in-javascript.js
@@ -0,0 +1,7 @@
+/*
+Set remainder equal to the remainder of 11 divided by 3 using the remainder (%) operator.
+*/
+
+// Only change code below this line
+
+var remainder = 5 % 3;
