9984aad5ef218336cb8e9613d975439e0cb41549
Randy <randyarbolaez0@gmail.com>
2019-09-06 11:26:02 -0400

    17-use-an-array-to-store-a-collection-of-data

---
+++ javascript-algorithims-and-data-structures/basic-data-structures/17-use-an-array-to-store-a-collection-of-data.js
@@ -0,0 +1,7 @@
+/*
+All array's have a length property, which as shown above, can be very easily accessed with the syntax Array.length.
+
+A more complex implementation of an array can be seen below. This is known as a multi-dimensional array, or an array that contains other arrays. Notice that this array also contains JavaScript objects, which we will examine very closely in our next section, but for now, all you need to know is that arrays are also capable of storing complex objects.
+*/
+
+let yourArray = [true, 5, 'hey', 0, 1]; // change this line
